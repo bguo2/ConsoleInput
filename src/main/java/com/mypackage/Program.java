b58243651269be94f6979c9c35bec0ffdc1bddc6
Jeff Guo <byguo2@yahoo.com>
2018-08-29 11:27:42 -0700

    new project

---
+++ src/main/java/com/mypackage/Program.java
@@ -0,0 +1,26 @@
+package com.mypackage;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input integer:");
+        int test1 = in.nextInt();
+        in.nextLine(); //eat the last cartridge return
+
+        System.out.println("Input string:");
+        String test4 = in.nextLine();
+
+        System.out.println("input float:");
+        float test2 = in.nextFloat();
+        in.nextLine(); //eat the last cartridge return
+
+        System.out.println(test1);
+        System.out.println(test2);
+        System.out.println(test4);
+    }
+}
---
+++ src/test/java/ConsoleTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertTrue;
+
+
+public class ConsoleTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
